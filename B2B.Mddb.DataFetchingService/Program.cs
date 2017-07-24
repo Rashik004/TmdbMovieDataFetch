2cbf2c396ae3c8af1bd3cbc491857816ac09b3d3
Rashik Hasnat <rashikhasnat@ymail.com>
2017-07-24 17:15:07 +0600

    Initiated Service project.

---
+++ B2B.Mddb.DataFetchingService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Mddb.DataFetchingService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            MddbDataFetchWindowsService myService=new MddbDataFetchWindowsService();
+            myService.OnDebug();
+#else
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+
+        }
+    }
+}
